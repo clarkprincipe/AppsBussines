ff3807d4ccca9eefa31b3cd48e0074e1b29909da
Clark Principe <clark@jemstep.com>
2018-07-20 10:30:30 -0500

    Login example bootstrap

--- target/scala-2.12/twirl/main/views/html/index.template.scala
+++ target/scala-2.12/twirl/main/views/html/index.template.scala
@@ -30,9 +30,23 @@ Seq[Any](format.raw/*6.1*/("""
 """),format.raw/*11.4*/("""
 """),_display_(/*12.2*/main("Welcome to Play", assetsFinder)/*12.39*/ {_display_(Seq[Any](format.raw/*12.41*/("""
 
-"""),format.raw/*14.1*/("""<h1>TItulo</h1>
+ """),format.raw/*14.2*/("""<form class="form-signin">
+  <img class="mb-4" src=""""),_display_(/*15.27*/assetsFinder/*15.39*/.path("images/bootstrap/bootstrap-solid.svg")),format.raw/*15.84*/("""" alt="" width="72" height="72">
+  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+  <label for="inputEmail" class="sr-only">Email address</label>
+  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+  <div class="checkbox mb-3">
+   <label>
+    <input type="checkbox" value="remember-me"> Remember me
+   </label>
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+</form>
 
-""")))}),format.raw/*16.2*/("""
+""")))}),format.raw/*30.2*/("""
 """))
       }
     }
@@ -49,11 +63,11 @@ Seq[Any](format.raw/*6.1*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Fri Jul 20 09:56:54 PET 2018
+                  DATE: Fri Jul 20 10:28:29 PET 2018
                   SOURCE: /home/clarkprincipe/AppsBussines/app/views/index.scala.html
-                  HASH: 96b993fdc9bbbdf377965b9b1cc6fc883945b2a9
-                  MATRIX: 873->137|1021->192|1049->387|1077->389|1123->426|1163->428|1192->430|1240->448
-                  LINES: 24->5|29->6|30->11|31->12|31->12|31->12|33->14|35->16
+                  HASH: de28ebc4d9b4a3c61df732c96f339de5ef56c3e6
+                  MATRIX: 873->137|1021->192|1049->387|1077->389|1123->426|1163->428|1193->431|1273->484|1294->496|1360->541|2084->1235
+                  LINES: 24->5|29->6|30->11|31->12|31->12|31->12|33->14|34->15|34->15|34->15|49->30
                   -- GENERATED --
               */
           
--- target/scala-2.12/twirl/main/views/html/main.template.scala
+++ target/scala-2.12/twirl/main/views/html/main.template.scala
@@ -17,32 +17,27 @@ import play.api.data._
 
 object main extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template3[String,AssetsFinder,Html,play.twirl.api.HtmlFormat.Appendable] {
 
-  /*
- * This template is called from the `index` template. This template
- * handles the rendering of the page header and body tags. It takes
- * three arguments, a `String` for the title of the page and an `Html`
- * object to insert into the body of the page and an `AssetFinder`
- * to define to reverse route static assets.
- */
-  def apply/*8.2*/(title: String, assetsFinder: AssetsFinder)(content: Html):play.twirl.api.HtmlFormat.Appendable = {
+  /**/
+  def apply/*1.2*/(title: String, assetsFinder: AssetsFinder)(content: Html):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 
 
-Seq[Any](format.raw/*9.1*/("""
-"""),format.raw/*10.1*/("""<!DOCTYPE html>
+Seq[Any](format.raw/*2.1*/("""
+"""),format.raw/*3.1*/("""<!DOCTYPE html>
 <html lang="en">
     <head>
-        """),format.raw/*13.62*/("""
-        """),format.raw/*14.9*/("""<title>"""),_display_(/*14.17*/title),format.raw/*14.22*/("""</title>
-        <link rel="stylesheet" media="screen" href=""""),_display_(/*15.54*/assetsFinder/*15.66*/.path("stylesheets/main.css")),format.raw/*15.95*/("""">
-        <link rel="shortcut icon" type="image/png" href=""""),_display_(/*16.59*/assetsFinder/*16.71*/.path("images/favicon.png")),format.raw/*16.98*/("""">
-        <script src=""""),_display_(/*17.23*/assetsFinder/*17.35*/.path("javascripts/hello.js")),format.raw/*17.64*/("""" type="text/javascript"></script>
+        <title>"""),_display_(/*6.17*/title),format.raw/*6.22*/("""</title>
+        <link rel="stylesheet" media="screen" href=""""),_display_(/*7.54*/assetsFinder/*7.66*/.path("stylesheets/bootstrap/bootstrap.css")),format.raw/*7.110*/("""">
+        <link rel="shortcut icon" type="image/png" href=""""),_display_(/*8.59*/assetsFinder/*8.71*/.path("images/favicon.png")),format.raw/*8.98*/("""">
+        <link rel="stylesheet" media="screen" href=""""),_display_(/*9.54*/assetsFinder/*9.66*/.path("stylesheets/custom/login.css")),format.raw/*9.103*/("""">
     </head>
-    <body>
-        """),format.raw/*21.32*/("""
-        """),_display_(/*22.10*/content),format.raw/*22.17*/("""
-    """),format.raw/*23.5*/("""</body>
+    <body class="text-center">
+        """),_display_(/*12.10*/content),format.raw/*12.17*/("""
+        """),format.raw/*13.9*/("""<script src=""""),_display_(/*13.23*/assetsFinder/*13.35*/.path("javascripts/jquery/jquery-3.3.1.min.js")),format.raw/*13.82*/("""" type="text/javascript"></script>
+        <script src=""""),_display_(/*14.23*/assetsFinder/*14.35*/.path("javascripts/bootstrap/bootstrap.js")),format.raw/*14.78*/("""" type="text/javascript"></script>
+    </body>
+
 </html>
 """))
       }
@@ -60,11 +55,11 @@ Seq[Any](format.raw/*9.1*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Fri Jul 20 09:51:49 PET 2018
+                  DATE: Fri Jul 20 10:23:14 PET 2018
                   SOURCE: /home/clarkprincipe/AppsBussines/app/views/main.scala.html
-                  HASH: 53adeab63e755599f15a4ff5273d97955b4dbce5
-                  MATRIX: 1067->327|1219->386|1247->387|1327->492|1363->501|1398->509|1424->514|1513->576|1534->588|1584->617|1672->678|1693->690|1741->717|1793->742|1814->754|1864->783|1958->939|1995->949|2023->956|2055->961
-                  LINES: 27->8|32->9|33->10|36->13|37->14|37->14|37->14|38->15|38->15|38->15|39->16|39->16|39->16|40->17|40->17|40->17|43->21|44->22|44->22|45->23
+                  HASH: 07e31ebf34af26bcfe405eb47fbf5a13b5334dcc
+                  MATRIX: 746->1|898->60|925->61|1011->121|1036->126|1124->188|1144->200|1209->244|1296->305|1316->317|1363->344|1445->400|1465->412|1523->449|1605->504|1633->511|1669->520|1710->534|1731->546|1799->593|1883->650|1904->662|1968->705
+                  LINES: 21->1|26->2|27->3|30->6|30->6|31->7|31->7|31->7|32->8|32->8|32->8|33->9|33->9|33->9|36->12|36->12|37->13|37->13|37->13|37->13|38->14|38->14|38->14
                   -- GENERATED --
               */
           
